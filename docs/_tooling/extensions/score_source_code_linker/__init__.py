00c539d85a87e09afbcc30c8b6872d086cb1d427
Alexander Lanin <Alexander.Lanin@etas.com>
2025-03-12 15:16:55 +0000

    WIP
    Co-authored-by: Maximilian Sören Pollak <MaximilianSoerenPollak@users.noreply.github.com>

--- docs/_tooling/extensions/score_source_code_linker/__init__.py
+++ docs/_tooling/extensions/score_source_code_linker/__init__.py
@@ -24,51 +24,15 @@ LOGGER = get_logger(__name__)
 
 
 def setup(app: Sphinx) -> dict:
-    """
-    Setup function needed to identify this file as a Sphinx-Extension
-
-    Args:
-        app: Sphinx app of the current running application. Populated automatically.
-
-    Returns:
-        (dict):
-            Return extension metadata.
-
-            {
-                "version": "0.1",
-                "parallel_read_safe": True,
-                "parallel_write_safe": True,
-            }
-    """
-    app.connect("env-updated", add_source_link)
     app.add_config_value("source_code_linker_file", "", rebuild="env")
-    print("===========")# TODO
-    print("LOADED SOURCE CODE LINKER")
-    print("===========")
+    if app.config.source_code_linker_file:
+        print("Loading source code linker...")
+        app.connect("env-updated", add_source_link)
     return {
         "version": "0.1",
         "parallel_read_safe": True,
         "parallel_write_safe": True,
     }
-
-
-def find_dir_paths(app: Sphinx) -> list[str]:
-    """
-    Reading 'requirement_links' config value and returning it as list
-    The 'requirement_links' config value contains all source links found
-    that need to be parsed.
-    Args:
-        app: Sphinx app of the current running application
-
-    Returns:
-        (list[str]):
-                List of filenames as strings captured from the 'requirement_links'
-                configuration value.
-
-        Example:
-            [file-1, file-2]
-    """
-    return [app.config.source_code_linker_file]
 
 
 def add_source_link(app: Sphinx, env) -> None:
@@ -84,38 +48,37 @@ def add_source_link(app: Sphinx, env) -> None:
     Needs_Data = SphinxNeedsData(env)
     needs = Needs_Data.get_needs_mutable()
     needs_copy = deepcopy(needs)
-    json_paths = find_dir_paths(app)
-    for path in json_paths:
-        try:
-            print(f"Opening {path} from CWD {os.getcwd()}, PWD {os.environ.get('PWD')}")
-            with open(path) as f:
-                gh_json = json.load(f)
-            for id, link in gh_json.items():
-                id = id.strip()
-                try:
-                    # NOTE: Removing & adding the need is important to make sure
-                    # the needs gets 're-evaluated'.
-                    need = needs_copy[id]  # NeedsInfoType
-                    Needs_Data.remove_need(need["id"])
-                    need["source_code_link"] = ",".join(link)
-                    print(f"NOW ADDING LINKS TO NEED: {need['id']}")
-                    Needs_Data.add_need(need)
-                except KeyError:
-                    # NOTE: manipulating link to remove git-hash,
-                    # making the output file location more readable
-                    files = [
-                        x.replace(GITHUB_BASE_URL, "").split("/", 1)[-1] for x in link
-                    ]
-                    LOGGER.warning(
-                        f"Could not find {id} in the needs id's. Found in file(s): {files}",
-                        type="score_source_code_linker",
-                    )
-        except Exception as e:
-            LOGGER.warning(
-                f"An unexpected error occurred while adding source_code_links to needs."
-                f"Error: {e}",
-                type="score_source_code_linker",
-            )
-            LOGGER.warning(
-                f"Reading file: {path} right now", type="score_source_code_linker"
-            )
+    path = app.config.source_code_linker_file
+    try:
+        print(f"Opening {path} from CWD {os.getcwd()}, PWD {os.environ.get('PWD')}")
+        with open(path, "w") as f:
+            gh_json = json.load(f)
+        for id, link in gh_json.items():
+            id = id.strip()
+            try:
+                # NOTE: Removing & adding the need is important to make sure
+                # the needs gets 're-evaluated'.
+                need = needs_copy[id]  # NeedsInfoType
+                Needs_Data.remove_need(need["id"])
+                need["source_code_link"] = ",".join(link)
+                print(f"NOW ADDING LINKS TO NEED: {need['id']}")
+                Needs_Data.add_need(need)
+            except KeyError:
+                # NOTE: manipulating link to remove git-hash,
+                # making the output file location more readable
+                files = [
+                    x.replace(GITHUB_BASE_URL, "").split("/", 1)[-1] for x in link
+                ]
+                LOGGER.warning(
+                    f"Could not find {id} in the needs id's. Found in file(s): {files}",
+                    type="score_source_code_linker",
+                )
+    except Exception as e:
+        LOGGER.warning(
+            f"An unexpected error occurred while adding source_code_links to needs."
+            f"Error: {e}",
+            type="score_source_code_linker",
+        )
+        LOGGER.warning(
+            f"Reading file: {path} right now", type="score_source_code_linker"
+        )
